0eac17a955e2491683e1be25901402484dc54a2c
Peter Zaika <peter.zaika@live.com>
2017-08-06 12:03:11 -0700

    Bugs

--- index.android.js
+++ index.android.js
@@ -9,46 +9,46 @@ import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 import FBLoginView from './components/FBLoginView.js';
 
 export default class Posterum extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Posterum
-        </Text>
-        <FBLogin
-            buttonView={<FBLoginView />}
-            ref={(fbLogin) => { this.fbLogin = fbLogin }}
-            loginBehavior={FBLoginManager.LoginBehaviors.Native}
-            permissions={["email","user_friends"]}
-            onLogin={function(e){console.log(e)}}
-            onLoginFound={function(e){console.log(e)}}
-            onLoginNotFound={function(e){console.log(e)}}
-            onLogout={function(e){console.log(e)}}
-            onCancel={function(e){console.log(e)}}
-            onPermissionsMissing={function(e){console.log(e)}}
-          />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to Posterum
+                </Text>
+                <FBLogin
+                    buttonView={<FBLoginView />}
+                    ref={(fbLogin) => { this.fbLogin = fbLogin }}
+                    loginBehavior={FBLoginManager.LoginBehaviors.Native}
+                    permissions={["email","user_friends"]}
+                    onLogin={function(e){console.log(e)}}
+                    onLoginFound={function(e){console.log(e)}}
+                    onLoginNotFound={function(e){console.log(e)}}
+                    onLogout={function(e){console.log(e)}}
+                    onCancel={function(e){console.log(e)}}
+                    onPermissionsMissing={function(e){console.log(e)}}
+                  />
+              </View>
+          );
+      }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('posterum', () => Posterum);
